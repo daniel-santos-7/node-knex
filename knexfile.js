5c83b122afb3a62aa781c9c5bade61e2c7bbfa7e
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-04-29 22:00:47 -0300

    commit inicial

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'node_knex',
+      user:     'docker',
+      password: 'docker',
+      host: '192.168.99.100'
+    },
+    migrations: {
+      directory: `${__dirname}/src/database/migrations`
+    }
+  }
+
+};
