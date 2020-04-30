5457f02006345c929fdd6f033535b78e121b5976
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-04-29 22:20:18 -0300

    tabela de usuários criada

---
+++ src/database/index.js
@@ -0,0 +1,5 @@
+const knexfile = require('../../knexfile');
+const kenx = require('knex');
+
+const db = kenx(knexfile.development);
+
--- src/database/migrations/20200429215932_create_users.js
+++ src/database/migrations/20200429215932_create_users.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
   
+    return knex.schema.createTable('users', (table)=> {
+
+        table.increments('id');
+        table.text('username').unique().notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+    });
+
 };
 
 exports.down = function(knex) {
-  
+    knex.schema.dropTable('users');
 };
